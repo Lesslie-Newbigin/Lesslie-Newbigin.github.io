e5314305cad42cc43196b14fbd44971c81e40b85
Lesslie Newbigin <lessliehkrl@gmail.com>
2025-05-07 21:13:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function login() {
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const error = document.getElementById("error");
+
+  if (username === "lesslie Newbigin" && password === "2aug0088l") {
+    document.getElementById("loginBox").style.display = "none";
+    document.getElementById("dashboard").style.display = "block";
+    error.textContent = "";
+  } else {
+    error.textContent = "Invalid credentials. Access denied.";
+  }
+}
